6ccca896be4012cb02270d9eb9337168f768844f
chhuovanna <chhuo.vanna87@gmail.com>
2019-07-19 15:03:00 +0700

    fix filter and sort

--- public/cozastore/js/main.js
+++ public/cozastore/js/main.js
@@ -536,7 +536,7 @@
     $(document).off('click','.sort-by');
     $(document).on('click','.sort-by', function(){
         var sort_by = $(this).data('sort');
-        var old_sort_by = $('.filter-link-active');
+        var old_sort_by = $('.filter-link-active.sort-by');
         old_sort_by.removeClass('filter-link-active');
         $(this).addClass('filter-link-active');
 
@@ -569,7 +569,7 @@
 /*===================================================================[ filter by ]*/
     $(document).off('click','.filter-by');
     $(document).on('click','.filter-by', function(){
-        var old_active = $('.filter-link-active');
+        var old_active = $('.filter-link-active.filter-by');
 
 
 
@@ -591,7 +591,7 @@
             
            
             if (isNaN(filter_by)){
-                $('.testoutput').append('<p>'+year+'</p>>' + '<p>'+filter_by+'</p>>'+'<p>'+active_director+'</p>>');
+              //  $('.testoutput').append('<p>'+year+'</p>>' + '<p>'+filter_by+'</p>>'+'<p>'+active_director+'</p>>');
                 if (filter_by  == 'all'){
                     if ( active_director == '')
                         return true;
